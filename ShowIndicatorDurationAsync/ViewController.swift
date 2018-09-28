e2a09b11d2974c199a5f619b871e888f7f377396
田辺信之 <tanabenobuyuki@tanabenobuyukinoMacBook-puro.local>
2018-09-28 17:54:55 +0900

    implement AlertView added UIActivityIndicatorView

--- ShowIndicatorDurationAsync/ViewController.swift
+++ ShowIndicatorDurationAsync/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var activityIndicatorView = UIActivityIndicatorView(style: .whiteLarge)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func tapSynchronouButton(_ sender: Any) {
+        let alert = UIAlertController.showIndicatorAlert(viewController: self, message: "uploading")
 
-
+        DispatchQueue.main.asyncAfter(deadline: .now() + 7.0) {
+            alert.message = "もう少しで終わるよ"
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                alert.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
 }
 
+
+
+extension UIAlertController {
+    class func showIndicatorAlert(viewController: UIViewController, message: String) -> UIAlertController {
+        let alert: UIAlertController = self.init(title: nil, message: message, preferredStyle: .alert)
+        // add indicator
+        let indicator = UIActivityIndicatorView(style: .gray)
+        indicator.center = CGPoint(x: 25.0, y: 30.0)
+        alert.view.addSubview(indicator)
+        DispatchQueue.main.async {
+            indicator.startAnimating()
+            viewController.present(alert, animated: true, completion: nil)
+        }
+        return alert
+    }
+}
